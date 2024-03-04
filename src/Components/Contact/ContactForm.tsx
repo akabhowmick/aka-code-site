36c5532ecb27785c5a42c588787168d491377946
akabhowmick <47453997+akabhowmick@users.noreply.github.com>
2024-03-04 11:27:00 -0500

    redo contact form

--- src/Components/Contact/ContactForm.tsx
+++ src/Components/Contact/ContactForm.tsx
@@ -1,112 +1,131 @@
-import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
-import FloatingLabel from "react-bootstrap/FloatingLabel";
-import Form from "react-bootstrap/Form";
-import * as formik from "formik";
-import * as yup from "yup";
-import Row from "react-bootstrap/esm/Row";
+import { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import emailjs from "@emailjs/browser";
 
 export const ContactForm = () => {
-  const { Formik } = formik;
-
-  const schema = yup.object().shape({
-    firstName: yup.string().required(),
-    lastName: yup.string().required(),
-    email: yup.string().email().required(),
-    message: yup.string().required(),
+  const [buttonState, setButtonState] = useState("Send Message");
+  const formik = useFormik({
+    initialValues: {
+      from_name: "", //user name
+      subject: "", // subject of email
+      reply_to: "", // user email
+      message: "", // message of email
+    },
+    validationSchema: Yup.object({
+      from_name: Yup.string().required("* Name field is required"),
+      subject: Yup.string().required("* Subject field is required"),
+      reply_to: Yup.string().email("Invalid email address").required("* Email field is required"),
+      message: Yup.string().required("* Message field is required"),
+    }),
+    onSubmit: (values, { setSubmitting, resetForm }) => {
+      try {
+        emailjs
+          .send("service_iodyf8e", "template_s9bux1j", values, "a8dh5CLKMF5OX1aLy")
+          .then(() => {
+            setButtonState("Send Email");
+            setSubmitting(false);
+            resetForm();
+          });
+      } catch {
+        setButtonState("Send Email");
+        setSubmitting(false);
+      }
+    },
   });
-
-  const handleFormSubmitClick = (e: {
-    firstName: string;
-    lastName: string;
-    email: string;
-    message: string;
-  }) => {
-    console.log("form submit", e.firstName);
-  };
-
   return (
-    <Formik
-      validationSchema={schema}
-      onSubmit={(e) => handleFormSubmitClick(e)}
-      initialValues={{
-        firstName: "",
-        lastName: "",
-        email: "",
-        message: "",
-      }}
-    >
-      {({ handleSubmit, handleChange, values, touched, errors }) => (
-        <Form onSubmit={handleSubmit}>
-          <Row className="mb-3">
-            <Form.Group as={Col}>
-              <FloatingLabel
-                controlId="firstName"
-                label="First name"
-                className="mb-3 form-input-label"
-              >
-                <Form.Control
-                  className="contact-form-input"
-                  type="text"
-                  name="firstName"
-                  value={values.firstName}
-                  onChange={handleChange}
-                  isValid={touched.firstName && !errors.firstName}
-                />
-              </FloatingLabel>
-              <Form.Control.Feedback type="invalid" tooltip>
-                {errors.firstName}
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col}>
-              <FloatingLabel controlId="validationFormik01" label="Last name" className="mb-3">
-                <Form.Control
-                  className="contact-form-input"
-                  type="text"
-                  name="lastName"
-                  value={values.lastName}
-                  onChange={handleChange}
-                  isValid={touched.lastName && !errors.lastName}
-                />
-              </FloatingLabel>
-              <Form.Control.Feedback type="invalid" tooltip>
-                {errors.lastName}
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col}>
-              <FloatingLabel controlId="email" label="Email Address" className="mb-3">
-                <Form.Control
-                  className="contact-form-input"
-                  type="text"
-                  name="email"
-                  value={values.email}
-                  onChange={handleChange}
-                  isValid={touched.email && !errors.email}
-                />
-              </FloatingLabel>
-              <Form.Control.Feedback type="invalid" tooltip>
-                {errors.email}
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Row>
-          <Form.Group as={Col}>
-            <FloatingLabel controlId="message" label="Comments">
-              <Form.Control
-                className="contact-form-input"
-                as="textarea"
-                style={{ height: "200px" }}
-                value={values.message}
-                onChange={handleChange}
-                isValid={touched.message && !errors.message}
-              />
-            </FloatingLabel>
-            <Form.Control.Feedback type="invalid" tooltip>
-              {errors.message}
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Button type="submit">Submit form</Button>
-        </Form>
-      )}
-    </Formik>
+    <form className="formcontact" onSubmit={formik.handleSubmit}>
+      <div className="contactform">
+        <div className="form-row-1">
+          <div className="contact-form-div">
+            <label htmlFor="from_name">Name:</label>
+            <input
+              className="contact-form-input"
+              id="from_name"
+              name="from_name"
+              type="text"
+              autoComplete="off"
+              placeholder="YOUR NAME"
+              onChange={formik.handleChange}
+              value={formik.values.from_name}
+            />
+            <div
+              className={`expandable ${
+                formik.touched.from_name && formik.errors.from_name ? "show" : ""
+              }`}
+            >
+              {formik.errors.from_name}
+            </div>
+          </div>
+          <div className="contact-form-div">
+            <label htmlFor="subject">Subject</label> 
+            <input
+              className="contact-form-input"
+              id="subject"
+              name="subject"
+              type="text"
+              autoComplete="off"
+              placeholder="SUBJECT"
+              onChange={formik.handleChange}
+              value={formik.values.subject}
+            />
+            <div
+              className={`expandable ${
+                formik.touched.subject && formik.errors.subject ? "show" : ""
+              }`}
+            >
+              {formik.errors.subject}
+            </div>
+          </div>
+          <div className="contact-form-div">
+            <label htmlFor="reply_to">Email</label> 
+            <input
+              className="contact-form-input"
+              id="reply_to"
+              type="email"
+              name="reply_to"
+              placeholder="YOUR EMAIL"
+              autoComplete="off"
+              onChange={formik.handleChange}
+              value={formik.values.reply_to}
+            />
+            <div
+              className={`expandable ${
+                formik.touched.reply_to && formik.errors.reply_to ? "show" : ""
+              }`}
+            >
+              {formik.errors.reply_to}
+            </div>
+          </div>
+        </div>
+        <div className="contact-message-div">
+          <label htmlFor="message">Message</label>
+          <textarea
+            className="contact-form-input"
+            id="message"
+            name="message"
+            placeholder="YOUR MESSAGE"
+            autoComplete="off"
+            onChange={formik.handleChange}
+            value={formik.values.message}
+          />
+          <div
+            className={`expandable ${
+              formik.touched.message && formik.errors.message ? "show" : ""
+            }`}
+          >
+            {formik.errors.message}
+          </div>
+        </div>
+        <div className="col-12">
+          <button disabled={formik.isSubmitting} type="submit" className="btn main-btn">
+            <span>{buttonState}</span>
+          </button>
+        </div>
+        <div className="col-12 form-message">
+          <span id="output" className="output_message text-center text-uppercase" />
+        </div>
+      </div>
+    </form>
   );
 };
